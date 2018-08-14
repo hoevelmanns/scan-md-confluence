952c9146879810cb2345ab80fc6e16ace6660380
Marcelle Hövelmanns <marcelle.hoevelmanns@affilicon.org>
2018-08-14 16:40:52 +0200

    refactors the build script

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@ echo "Building scripts...";
 
 DIR=`dirname $0`
 
-babel $DIR/src/scanMdConfluence.js --out-file $DIR/dist/scanMdConfluence.js
-babel $DIR/src/utils.js --out-file $DIR/dist/utils.js
-babel $DIR/src/page.js --out-file $DIR/dist/page.js
-babel $DIR/src/config.js --out-file $DIR/dist/config.js
+babel $DIR/src/*.js --out-file $DIR/dist/scanMdConfluence.js
 
 echo "Scripts successfully build!"
--- dist/scanMdConfluence.js
+++ dist/scanMdConfluence.js
@@ -3,6 +3,244 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.Config = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _utils = require("./utils");
+
+var _utils2 = _interopRequireDefault(_utils);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Config = exports.Config = function () {
+  function Config(file) {
+    _classCallCheck(this, Config);
+
+    this.utils = new _utils2.default();
+
+    if (!Config.instance) {
+      this.load(file);
+
+      Config.instance = this;
+    }
+
+    return Config.instance;
+  }
+
+  _createClass(Config, [{
+    key: "load",
+    value: function load(file) {
+
+      if (!file) {
+        return this.utils.displayError("Error: You must specify the configuration file with the parameter '--config=/path/configuration.json'");
+      }
+
+      try {
+
+        this._data = require(file);
+      } catch (e) {
+
+        return this.utils.displayError("Error: The specified configuration file was not found.", e);
+      }
+    }
+  }, {
+    key: "setPassword",
+    value: function setPassword(password) {
+      this._data.confluence.password = password;
+    }
+  }, {
+    key: "data",
+    value: function data() {
+      return this._data;
+    }
+  }, {
+    key: "isValid",
+    value: function isValid() {
+      var _this = this;
+
+      var required = ['confluence', 'scanDirectory', 'confluence.username', 'confluence.password', 'confluence.baseUrl', 'confluence.version', 'confluence.space', 'confluence.parentPageId'],
+          missing = [];
+
+      var splittedKey = void 0;
+
+      required.forEach(function (key) {
+
+        splittedKey = key.split(".");
+
+        if (splittedKey.length === 1) {
+          !_this._data[key] ? missing.push(key) : missing;
+        } else if (splittedKey.length === 2) {
+          !_this._data[splittedKey[0]][splittedKey[1]] ? missing.push(key) : missing;
+        }
+      });
+
+      if (missing.length) {
+
+        this.utils.displayError("Configuration ist not valid! Keys missing: ", missing.join(", "));
+      }
+
+      return missing.length === 0;
+    }
+  }]);
+
+  return Config;
+}();
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ConfluencePage = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _utils = require('./utils');
+
+var _utils2 = _interopRequireDefault(_utils);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ConfluencePage = exports.ConfluencePage = function () {
+  function ConfluencePage(confluenceApi, data, config) {
+    _classCallCheck(this, ConfluencePage);
+
+    this.markdown2confluence = require('markdown2confluence-cws');
+    this.confluenceApi = confluenceApi;
+    this.config = config;
+    this.utils = new _utils2.default();
+    this.labels = [];
+
+    if (!data || !data.hasOwnProperty('results')) return;
+
+    var resultData = data.results[0];
+
+    this.version = resultData.version.number;
+    this.content = resultData.body.storage.value;
+    this.title = resultData.title;
+    this.id = resultData.id;
+  }
+
+  _createClass(ConfluencePage, [{
+    key: 'setLabels',
+    value: function setLabels(labels) {
+      this.labels = labels;
+      return this;
+    }
+  }, {
+    key: 'setVersion',
+    value: function setVersion(version) {
+      this.version = version;
+      return this;
+    }
+  }, {
+    key: 'getVersion',
+    value: function getVersion() {
+      return this.version;
+    }
+  }, {
+    key: 'setTitle',
+    value: function setTitle(title) {
+      this.title = title;
+      return this;
+    }
+  }, {
+    key: 'getTitle',
+    value: function getTitle() {
+      return this.title;
+    }
+  }, {
+    key: 'setContent',
+    value: function setContent(content) {
+      this.content = this.markdown2confluence(content, this.config.confluence.markDown);
+
+      return this;
+    }
+  }, {
+    key: 'getContent',
+    value: function getContent() {
+      return this.content;
+    }
+  }, {
+    key: 'setId',
+    value: function setId(id) {
+      this.id = id;
+      return this;
+    }
+  }, {
+    key: 'getId',
+    value: function getId() {
+      return this.id;
+    }
+  }, {
+    key: 'create',
+    value: async function create() {
+      var _this = this;
+
+      return new Promise(function (resolve, reject) {
+        _this.confluenceApi.postContent(_this.config.confluence.space, _this.title, _this.content, _this.config.confluence.parentPageId, function (err, data) {
+
+          if (err || data.body.statusCode === 400) {
+            return reject(err);
+          }
+
+          _this.setId(data.id);
+
+          _this.utils.displaySuccess("Page created: ", _this.id + ", " + _this.title);
+
+          return _this.addLabels().then(resolve).catch(reject);
+        }, 'wiki');
+      });
+    }
+  }, {
+    key: 'update',
+    value: async function update() {
+      var _this2 = this;
+
+      return new Promise(function (resolve, reject) {
+        // page exist, so update this
+        _this2.confluenceApi.putContent(_this2.config.confluence.space, _this2.id, _this2.version, _this2.title, _this2.content, function (err) {
+
+          if (err) {
+            reject(err);
+          }
+
+          _this2.utils.displayInfo("Page updated: ", _this2.id + ", " + _this2.title);
+
+          return _this2.addLabels().then(resolve).catch(reject);
+        }, false, 'wiki');
+      });
+    }
+  }, {
+    key: 'addLabels',
+    value: async function addLabels() {
+      var _this3 = this;
+
+      if (!this.id || !this.labels || !this.labels.length) return;
+
+      return new Promise(function (resolve, reject) {
+        _this3.confluenceApi.postLabels(_this3.id, _this3.labels, function (data) {
+          _this3.utils.displayInfo("Labels created/updated for page " + _this3.id + ": ", _this3.labels.map(function (label) {
+            return label.name;
+          }).join(', '));
+
+          resolve(data);
+        });
+      });
+    }
+  }]);
+
+  return ConfluencePage;
+}();
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 exports.ScanMdConfluence = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -185,3 +423,101 @@ var ScanMdConfluence = exports.ScanMdConfluence = function () {
 }();
 
 var scanMdConfluence = module.exports = ScanMdConfluence;
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var fs = require('fs'),
+    colors = require('colors'),
+    klaw = require('klaw'),
+    path = require('path');
+
+var Utils = function () {
+  function Utils() {
+    _classCallCheck(this, Utils);
+  }
+
+  _createClass(Utils, [{
+    key: 'readFile',
+    value: async function readFile(path) {
+
+      return new Promise(function (resolve, reject) {
+        fs.readFile(path, 'utf8', function (err, data) {
+
+          if (err) {
+            reject(err);
+          }
+
+          resolve(data);
+        });
+      });
+    }
+  }, {
+    key: 'scanMarkdowns',
+    value: function scanMarkdowns(directory) {
+
+      var files = [];
+
+      return new Promise(function (resolve, reject) {
+        klaw(directory).on('data', function (item) {
+
+          if (path.extname(item.path) !== ".md") {
+            return;
+          }
+
+          files.push(item.path);
+        }).on('end', function () {
+          return resolve(files);
+        }).on('error', function () {
+          return reject;
+        });
+      });
+    }
+  }, {
+    key: 'displayError',
+    value: function displayError(message) {
+      var values = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
+
+      if (!values) {
+        values = "";
+      }
+
+      console.error(colors.red(String.fromCharCode("0x2718") + " " + message), colors.bold(values));
+    }
+  }, {
+    key: 'displaySuccess',
+    value: function displaySuccess(message) {
+      var values = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
+
+      if (!values) {
+        values = "";
+      }
+
+      console.info(colors.green(String.fromCharCode("0x2705") + " " + message), colors.bold(values));
+    }
+  }, {
+    key: 'displayInfo',
+    value: function displayInfo(message) {
+      var values = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
+
+      if (!values) {
+        values = "";
+      }
+
+      console.info(colors.yellow(String.fromCharCode("0x2705") + " " + message), colors.bold(values));
+    }
+  }]);
+
+  return Utils;
+}();
+
+exports.default = Utils;
